48c7b950ac1c6adf37437b43ea7f7d6cebc18cbb
David Reinartz <d.c.reinartz@gmail.com>
2026-06-08 10:20:25 +0200

    Add proper Footer, refactor layouts

---
+++ src/data/navigation.ts
@@ -0,0 +1,6 @@
+export const navigationItems = [
+  { href: "/", label: "Bienvenue!" },
+  { href: "/balfolk/", label: "Was? Bal Folk?" },
+  { href: "/code-of-conduct/", label: "Code of Conduct" },
+  { href: "/vielfalt-staerken/", label: "Vielfalt stärken!" },
+];
